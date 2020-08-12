e1a84b98e6dc67869472ea1b0aa09919bbe53153
Vinicius Reis <viniciusreis81@hotmail.com>
2020-08-12 20:57:14 -0300

    config: add database config

---
+++ src/database/config.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+module.exports = {
+  dialect: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  define: {
+    timestamps: true,
+    underscored: true
+  }
+}
