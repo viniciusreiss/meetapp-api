9b4d57ed0808c3115adadde34a9d955c7c916fff
Vinicius Reis <viniciusreis81@hotmail.com>
2020-08-27 11:22:35 -0300

    database: add database connection

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import './loadenv'
 import Express from 'express'
+import { isDbConnected } from '@database/sequelize'
+import routes from '@routes/index'
 
 const app = Express()
 
@@ -11,14 +14,17 @@ app.use(Express.json())
  * routes
  */
 
+app.use('/api', routes)
+
 /**
  * server initialization
  */
 const initServer = async () => {
   try {
-    app.listen(4000, () => console.log('server running'))
+    await isDbConnected()
+    app.listen(4000, () => console.log('server running on localhost:4000'))
   } catch (error) {
-    console.log('Error!')
+    console.log('Error!', error)
   }
 }
 
